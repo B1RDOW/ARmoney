d041357cc86720cd30d30408912b0970bfde67c0
BIRDOW <61405779+B1RDOW@users.noreply.github.com>
2022-01-14 15:09:26 +0600

    Delete src directory

--- src/main/java/com/birdow/armoney/utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.birdow.armoney.utils;
-
-import org.bukkit.ChatColor;
-
-public class Utils {
-    public static String color (String msg) {
-        return ChatColor.translateAlternateColorCodes('&', msg);
-    }
-}
